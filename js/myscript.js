3bc71dcdcd3771aa09ba5c85b345b70a635d4ece
Donatella7 <campaniellodonatella@gmail.com>
2022-02-25 15:15:38 +0100

    click event

--- js/myscript.js
+++ js/myscript.js
@@ -7,6 +7,8 @@ const outWinner = document.getElementById("output-winner");
 
 const arrayDiceOne = [];
 const arrayDiceTwo = [];
+let diceOne = Math.floor((Math.random() * 6) + 1);
+let diceTwo = Math.floor((Math.random() * 6) + 1);
 
 for(i = 0; i<arrayEmail.length; i++){
     if(arrayEmail[i] == inputUserEmail){
@@ -16,30 +18,41 @@ for(i = 0; i<arrayEmail.length; i++){
     }
 }
 
-let countWinOne = 0;
-let countWinTwo = 0;
+const btnOne = document.getElementById("my-dice-btn-one");
+btnOne.addEventListener("click", function(){
+    arrayDiceOne.push(diceOne);
+    outputRandomOne.innerHTML = (arrayDiceOne);
+});
 
-let diceOne = Math.floor((Math.random() * 6) + 1);
-arrayDiceOne.push(diceOne);
-outputRandomOne.innerHTML = (arrayDiceOne);
+const btnTwo = document.getElementById("my-dice-btn-two");
+btnTwo.addEventListener("click", function(){
+    arrayDiceTwo.push(diceTwo);
+    outputRandomTwo.innerHTML = (arrayDiceTwo);
+});
 
-let diceTwo = Math.floor((Math.random() * 6) + 1);
-arrayDiceTwo.push(diceTwo);
-outputRandomTwo.innerHTML = (arrayDiceTwo);
 
-if(diceOne > diceTwo){
-    countWinOne = countWinOne + 1;
-} else if (diceOne < diceTwo){
-    countWinTwo = countWinTwo + 1;
-} else{
-    countWinOne = countWinOne;
-    countWinTwo = countWinTwo;
-}
+const scoreBtn = document.getElementById("my-score");
+scoreBtn.addEventListener("click", function(){
+    
+    let countWinOne = 0;
+    let countWinTwo = 0;
+    
+    
+    if(diceOne > diceTwo){
+        countWinOne = countWinOne + 1;
+    } else if (diceOne < diceTwo){
+        countWinTwo = countWinTwo + 1;
+    } else{
+        countWinOne = countWinOne;
+        countWinTwo = countWinTwo;
+    }
+    
+    if ( countWinOne > countWinTwo){
+        outWinner.innerHTML = "ha vinto player 1";
+    } else if(countWinOne < countWinTwo){
+        outWinner.innerHTML = "ha vinto player 1";
+    } else{
+        outWinner.innerHTML = "Pari";
+    }
+});
 
-if ( countWinOne > countWinTwo){
-    outWinner.innerHTML = "ha vinto player 1";
-} else if(countWinOne < countWinTwo){
-    outWinner.innerHTML = "ha vinto player 1";
-} else{
-    outWinner.innerHTML = "Pari";
-}
