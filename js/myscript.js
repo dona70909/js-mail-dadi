5cee68f6ee6ad776493aec07a79fd045858287fe
Donatella7 <campaniellodonatella@gmail.com>
2022-02-25 15:40:45 +0100

    modifica if email - se l'email è giusta gioco.

--- js/myscript.js
+++ js/myscript.js
@@ -4,55 +4,63 @@ const outputCheckEmail  = document.getElementById("output-email-check");
 const outputRandomOne = document.getElementById("output-one");
 const outputRandomTwo = document.getElementById("output-two");
 const outWinner = document.getElementById("output-winner");
-
 const arrayDiceOne = [];
 const arrayDiceTwo = [];
 let diceOne = Math.floor((Math.random() * 6) + 1);
 let diceTwo = Math.floor((Math.random() * 6) + 1);
 
+
+
 for(i = 0; i<arrayEmail.length; i++){
     if(arrayEmail[i] == inputUserEmail){
+
+        /* se l'email e' giusta */
         outputCheckEmail.innerHTML = "yes";
-    } else{
-        outputCheckEmail.innerHTML = "nope";
+        /* gioca */
+        const btnOne = document.getElementById("my-dice-btn-one");
+        btnOne.addEventListener("click", function(){
+            arrayDiceOne.push(diceOne);
+            outputRandomOne.innerHTML = (arrayDiceOne);
+        });
+        
+        const btnTwo = document.getElementById("my-dice-btn-two");
+        btnTwo.addEventListener("click", function(){
+            arrayDiceTwo.push(diceTwo);
+            outputRandomTwo.innerHTML = (arrayDiceTwo);
+        });
+        
+        
+        const scoreBtn = document.getElementById("my-score");
+        scoreBtn.addEventListener("click", function(){
+            let countWinOne = 0;
+            let countWinTwo = 0;
+            
+            
+            if(diceOne > diceTwo){
+                countWinOne = countWinOne + 1;
+            } else if (diceOne < diceTwo){
+                countWinTwo = countWinTwo + 1;
+            } else{
+                countWinOne = countWinOne;
+                countWinTwo = countWinTwo;
+            }
+            
+            if ( countWinOne > countWinTwo){
+                outWinner.innerHTML = "ha vinto player 1";
+            } else if(countWinOne < countWinTwo){
+                outWinner.innerHTML = "ha vinto player 1";
+            } else{
+                outWinner.innerHTML = "Pari";
+            }
+        });
+
+    } else {
+
+        outputCheckEmail.innerHTML = "";
     }
 }
 
-const btnOne = document.getElementById("my-dice-btn-one");
-btnOne.addEventListener("click", function(){
-    arrayDiceOne.push(diceOne);
-    outputRandomOne.innerHTML = (arrayDiceOne);
-});
-
-const btnTwo = document.getElementById("my-dice-btn-two");
-btnTwo.addEventListener("click", function(){
-    arrayDiceTwo.push(diceTwo);
-    outputRandomTwo.innerHTML = (arrayDiceTwo);
-});
 
 
-const scoreBtn = document.getElementById("my-score");
-scoreBtn.addEventListener("click", function(){
-    
-    let countWinOne = 0;
-    let countWinTwo = 0;
-    
-    
-    if(diceOne > diceTwo){
-        countWinOne = countWinOne + 1;
-    } else if (diceOne < diceTwo){
-        countWinTwo = countWinTwo + 1;
-    } else{
-        countWinOne = countWinOne;
-        countWinTwo = countWinTwo;
-    }
-    
-    if ( countWinOne > countWinTwo){
-        outWinner.innerHTML = "ha vinto player 1";
-    } else if(countWinOne < countWinTwo){
-        outWinner.innerHTML = "ha vinto player 1";
-    } else{
-        outWinner.innerHTML = "Pari";
-    }
-});
+
 
